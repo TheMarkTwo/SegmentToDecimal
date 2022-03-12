f375120d5d3645d730276d3f61f0bb23e919992e
TheMarkTwo <mlovreko123@gmail.com>
2022-03-12 11:18:22 +0100

    Shiftanje lijevo i desno

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -29,34 +29,91 @@ namespace SegmentToDecimal
         /// </summary>
         private void InitializeComponent()
         {
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.rtxtInfo = new System.Windows.Forms.RichTextBox();
             this.pnlGrid = new System.Windows.Forms.Panel();
+            this.btnShiftLeft = new System.Windows.Forms.Button();
+            this.btnShiftRight = new System.Windows.Forms.Button();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.btnOcisti = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // richTextBox1
+            // rtxtInfo
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(12, 385);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(441, 157);
-            this.richTextBox1.TabIndex = 1;
-            this.richTextBox1.Text = "";
+            this.rtxtInfo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.rtxtInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtxtInfo.Location = new System.Drawing.Point(12, 261);
+            this.rtxtInfo.Name = "rtxtInfo";
+            this.rtxtInfo.ReadOnly = true;
+            this.rtxtInfo.Size = new System.Drawing.Size(448, 75);
+            this.rtxtInfo.TabIndex = 60;
+            this.rtxtInfo.Text = "";
             // 
             // pnlGrid
             // 
             this.pnlGrid.BackColor = System.Drawing.Color.Silver;
             this.pnlGrid.Location = new System.Drawing.Point(1, 0);
             this.pnlGrid.Name = "pnlGrid";
-            this.pnlGrid.Size = new System.Drawing.Size(466, 160);
-            this.pnlGrid.TabIndex = 2;
+            this.pnlGrid.Size = new System.Drawing.Size(470, 206);
+            this.pnlGrid.TabIndex = 0;
+            // 
+            // btnShiftLeft
+            // 
+            this.btnShiftLeft.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnShiftLeft.Location = new System.Drawing.Point(12, 212);
+            this.btnShiftLeft.Name = "btnShiftLeft";
+            this.btnShiftLeft.Size = new System.Drawing.Size(75, 36);
+            this.btnShiftLeft.TabIndex = 3;
+            this.btnShiftLeft.Text = "<";
+            this.btnShiftLeft.UseVisualStyleBackColor = true;
+            this.btnShiftLeft.Click += new System.EventHandler(this.btnShiftLeft_Click);
+            // 
+            // btnShiftRight
+            // 
+            this.btnShiftRight.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnShiftRight.Location = new System.Drawing.Point(385, 212);
+            this.btnShiftRight.Name = "btnShiftRight";
+            this.btnShiftRight.Size = new System.Drawing.Size(75, 36);
+            this.btnShiftRight.TabIndex = 4;
+            this.btnShiftRight.Text = ">";
+            this.btnShiftRight.UseVisualStyleBackColor = true;
+            this.btnShiftRight.Click += new System.EventHandler(this.btnShiftRight_Click);
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.BackColor = System.Drawing.Color.Transparent;
+            this.btnGenerate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGenerate.Location = new System.Drawing.Point(93, 212);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(286, 36);
+            this.btnGenerate.TabIndex = 61;
+            this.btnGenerate.Text = "Generiraj kod";
+            this.btnGenerate.UseVisualStyleBackColor = false;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // btnOcisti
+            // 
+            this.btnOcisti.BackColor = System.Drawing.Color.Transparent;
+            this.btnOcisti.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOcisti.Location = new System.Drawing.Point(12, 350);
+            this.btnOcisti.Name = "btnOcisti";
+            this.btnOcisti.Size = new System.Drawing.Size(448, 35);
+            this.btnOcisti.TabIndex = 62;
+            this.btnOcisti.Text = "Ocisti";
+            this.btnOcisti.UseVisualStyleBackColor = false;
+            this.btnOcisti.Click += new System.EventHandler(this.btnOcisti_Click);
             // 
             // FrmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightGray;
-            this.ClientSize = new System.Drawing.Size(467, 554);
+            this.ClientSize = new System.Drawing.Size(472, 397);
+            this.Controls.Add(this.btnOcisti);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.btnShiftRight);
+            this.Controls.Add(this.btnShiftLeft);
             this.Controls.Add(this.pnlGrid);
-            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.rtxtInfo);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Margin = new System.Windows.Forms.Padding(4);
@@ -70,8 +127,12 @@ namespace SegmentToDecimal
         }
 
         #endregion
-        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox rtxtInfo;
         private System.Windows.Forms.Panel pnlGrid;
+        private System.Windows.Forms.Button btnShiftLeft;
+        private System.Windows.Forms.Button btnShiftRight;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Button btnOcisti;
     }
 }
 
--- MainForm.cs
+++ MainForm.cs
@@ -23,23 +23,27 @@ namespace SegmentToDecimal
             {
                 for (int j = 0; j < 7; j++)
                 {
-                    CheckBox c = new CheckBox() 
+                    CheckBox c = new CheckBox()
                     {
-                        BackColor = Color.DimGray,
-                        Size = new Size(14, 15),
+                        Appearance = Appearance.Button,
+                        FlatStyle = FlatStyle.Flat,
+                        Size = new Size(23, 23),
                         Name = $"i{i:D2}j{j}",
-                        Location = new Point(i * 18 + 15, 18 * j + 15)
+                        Location = new Point(i * 25 + 15, 25 * j + 15)
                     };
+                    c.FlatAppearance.CheckedBackColor = Color.Red;
                     c.CheckedChanged += CheckedChanged;
                     pnlGrid.Controls.Add(c);
 
                     if (!c.Name.Contains("i14")) continue;
                     Label l = new Label()
                     {
-                        Size = new Size(50, 15),
+                        Font = new Font("Arial", 11),
+                        ForeColor = Color.DarkSlateGray,
+                        Size = new Size(50, 25),
                         Text = "0",
                         Name = $"row{j}",
-                        Location = new Point(i * 20 + 15, 18 * j + 15)
+                        Location = new Point(i * 28 + 10, 25 * j + 18)
                     };
                     pnlGrid.Controls.Add(l);
                 }
@@ -53,21 +57,98 @@ namespace SegmentToDecimal
             int j = int.Parse(c.Name[4].ToString());
 
             Label l = pnlGrid.Controls[$"row{j}"] as Label;
-            int rowValue = int.Parse(l.Text);
+            ushort rowValue = ushort.Parse(l.Text);
 
             if (c.Checked == true)
-            {
-                rowValue += (int)Math.Pow(2, 14 - i);
-
-                richTextBox1.Text = $"cbox: {c.Name} \n i = {i} \n j = {j} \n\n lbl: {l.Name} \n {rowValue}";
-            }
+                rowValue += (ushort)Math.Pow(2, 14 - i);
             else
-            {
-                rowValue -= (int)Math.Pow(2, 14 - i);
-                
-                richTextBox1.Text = "";
-            }
+                rowValue -= (ushort)Math.Pow(2, 14 - i);
+
             l.Text = rowValue.ToString();
+            //rtxtInfo.Text = $"cbox: {c.Name} \n i = {i} \n j = {j} \n\n lbl: {l.Name} \n {rowValue}";
+        }
+
+        private void btnShiftLeft_Click(object sender, EventArgs e)
+        {
+            bool[,] grid = GridToArray3D(pnlGrid);
+            for (int i = 0; i < 15; i++)
+            {
+                for (int j = 0; j < 7; j++)
+                {
+                    if (i != 14)
+                        grid[i, j] = grid[i + 1, j];
+                    else
+                        grid[i, j] = false;
+                }
+            }
+            Array3DToGrid(grid);
+        }
+
+        private void btnShiftRight_Click(object sender, EventArgs e)
+        {
+            bool[,] grid = GridToArray3D(pnlGrid);
+            for (int i = 14; i >= 0; i--)
+            {
+                for (int j = 6; j >= 0; j--)
+                {
+                    if (i != 0)
+                        grid[i, j] = grid[i - 1, j];
+                    else
+                        grid[i, j] = false;
+                }
+            }
+            Array3DToGrid(grid);
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            Label l;
+            string data = "DATA ";
+            for (int i = 0; i < 7; i++)
+            {
+                l = pnlGrid.Controls[$"row{i}"] as Label;
+                data += l.Text;
+                if (i != 6) data += ", ";
+            }
+            rtxtInfo.Text = data;
+        }
+
+        private void btnOcisti_Click(object sender, EventArgs e)
+        {
+            foreach (Control c in pnlGrid.Controls)
+            {
+                if (c is CheckBox box)
+                    box.Checked = false;
+            }
+            rtxtInfo.Text = "";
+        }
+
+        private bool[,] GridToArray3D(Panel panel)
+        {
+            bool[,] grid = new bool[15, 7];
+            foreach (Control c in pnlGrid.Controls)
+            {
+                if (c is CheckBox box)
+                {
+                    int i = int.Parse(c.Name.Substring(1, 2));
+                    int j = int.Parse(c.Name[4].ToString());
+                    grid[i, j] = box.Checked;
+                }
+            }
+            return grid;
+        }
+
+        private void Array3DToGrid(bool[,] grid)
+        {
+            CheckBox c;
+            for (int i = 0; i < 15; i++)
+            {
+                for (int j = 0; j < 7; j++)
+                {
+                    c = pnlGrid.Controls[$"i{i:D2}j{j}"] as CheckBox;
+                    c.Checked = grid[i,j];
+                }
+            }
         }
     }
 }
