0a9b3001f251d319f6dbeebeec2ca85c20748485
TheMarkTwo <mlovreko123@gmail.com>
2022-03-12 09:02:00 +0100

    Checkbox grid generation

---
+++ MainForm.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace SegmentToDecimal
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.pnlGrid = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(206, 313);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(630, 157);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // pnlGrid
+            // 
+            this.pnlGrid.BackColor = System.Drawing.Color.DarkGray;
+            this.pnlGrid.Location = new System.Drawing.Point(1, 0);
+            this.pnlGrid.Name = "pnlGrid";
+            this.pnlGrid.Size = new System.Drawing.Size(960, 295);
+            this.pnlGrid.TabIndex = 2;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.pnlGrid);
+            this.Controls.Add(this.richTextBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "FrmMain";
+            this.ShowIcon = false;
+            this.Text = "SegmentToDecimal";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Panel pnlGrid;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SegmentToDecimal
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 15; i++)
+            {
+                for (int j = 0; j < 7; j++)
+                {
+                    CheckBox c = new CheckBox() 
+                    {
+                        BackColor = Color.DimGray,
+                        Size = new Size(14, 15),
+                        Name = $"i{i}j{j}",
+                        Location = new Point(i * 18 + 15, 18 * j + 15)
+                    };
+                    c.CheckedChanged += CheckedChanged;
+                    pnlGrid.Controls.Add(c);
+
+                    if (!c.Name.Contains("i14")) continue;
+                    Label l = new Label()
+                    {
+                        Size = new Size(50, 15),
+                        Text = "0",
+                        Name = $"row{j}",
+                        Location = new Point(i * 20 + 15, 18 * j + 15)
+                    };
+                    pnlGrid.Controls.Add(l);
+                }
+            }
+        }
+
+        private void CheckedChanged(object sender, EventArgs e)
+        {
+            richTextBox1.Text = ((CheckBox)sender).Name + "    " + ((CheckBox)sender).Location.X;
+        }
+    }
+}
